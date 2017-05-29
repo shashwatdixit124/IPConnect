510e66b2f002673be6f9de3a3418f15f2453931b
Shashwat Dixit <shashwatdixit124@gmail.com>
2017-05-30 03:00:06 +0530

    interface for Downloader that handles a file transfer either sending or receiving

---
+++ src/lib/interfaces/idownloader.h
@@ -0,0 +1,44 @@
+/*
+ *   This file is part of IPConnect
+ *   Copyright 2017 Shashwat Dixit <shashwatdixit124@gmail.com>
+ * 
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 3 of the
+ *   License, or (at your option) any later version.
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef IDOWNLOADER_H
+#define IDOWNLOADER_H
+
+#include <QObject>
+
+namespace IPConnect
+{
+
+class IDownloader : public QObject
+{
+	Q_OBJECT
+
+public:
+	virtual void start() = 0;
+
+protected:
+	explicit IDownloader(QObject* parent = nullptr);
+	~IDownloader();
+
+};
+
+}
+
+#endif
