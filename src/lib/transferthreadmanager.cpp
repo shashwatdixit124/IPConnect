77d6ebac6d6b620d01db3fd309d9e605685016e5
Shashwat Dixit <shashwatdixit124@gmail.com>
2017-06-04 17:24:25 +0530

    TransferThreadManager will manage all created transfers in different thread and will pass their status to TransferManager

---
+++ src/lib/transferthreadmanager.cpp
@@ -0,0 +1,111 @@
+/*
+ *   This file is part of IPConnect
+ *   Copyright 2017 Shashwat Dixit <shashwatdixit124@gmail.com>
+ * 
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 3 of the
+ *   License, or (at your option) any later version.
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "transferthreadmanager.h"
+
+#include "transfer.h"
+#include "debugtransfer.h"
+
+#include <QObject>
+
+namespace IPConnect
+{
+
+TransferThreadManager::TransferThreadManager(QObject* parent) : QObject(parent)
+{
+}
+
+TransferThreadManager::~TransferThreadManager()
+{
+	foreach(Transfer* t , m_transferList.values())
+		removeTransfer(t);
+}
+
+void TransferThreadManager::transferCreated(Transfer* transfer)
+{
+	if(!transfer)
+		return;
+
+	connect(transfer,&Transfer::requested,this,&TransferThreadManager::requested);
+	connect(transfer,&Transfer::destroyTransfer,this,&TransferThreadManager::destroyTransfer);
+	qint16 id = transfer->file().id();
+	m_transferList.insert(id,transfer);
+	qCDebug(TRANSFER) << this << "Transfer Added with id " << id ;
+}
+
+void TransferThreadManager::manualTransferCreated(Transfer* transfer)
+{
+	if(!transfer)
+		return;
+
+	qint16 id = transfer->file().id();
+	m_transferList.insert(id,transfer);
+	qCDebug(TRANSFER) << this << "Manual Transfer Added with id " << id;
+}
+
+void TransferThreadManager::acceptPending(qint16 id)
+{
+	Transfer* t = m_transferList.value(id);
+	if(!t)
+		return;
+	t->accept();
+	emit accepted(id);
+}
+
+void TransferThreadManager::rejectPending(qint16 id)
+{
+	Transfer* t = m_transferList.value(id);
+	if(!t)
+		return;
+	t->reject();
+	emit rejected(id);
+}
+
+void TransferThreadManager::requested()
+{
+	if(!sender())
+		return;
+
+	Transfer* t = dynamic_cast<Transfer*>(sender());
+	File f = t->file();
+	qint16 id = f.id();
+	m_transferList.value(id)->setFile(f);
+	emit requestedTransfer(f);
+}
+
+void TransferThreadManager::destroyTransfer()
+{
+	if(!sender())
+		return;
+
+	Transfer* t = dynamic_cast<Transfer*>(sender());
+	removeTransfer(t);
+}
+
+void TransferThreadManager::removeTransfer(Transfer* t)
+{
+	QMetaObject::invokeMethod(t,"moveToThread",Qt::QueuedConnection,Q_ARG(QThread*,thread()));
+	qint16 id = t->file().id();
+	t->deleteLater();
+	m_transferList.remove(id);
+	emit transferRemoved(id);
+}
+
+}
---
+++ src/lib/transferthreadmanager.h
@@ -0,0 +1,66 @@
+/*
+ *   This file is part of IPConnect
+ *   Copyright 2017 Shashwat Dixit <shashwatdixit124@gmail.com>
+ * 
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 3 of the
+ *   License, or (at your option) any later version.
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef TRANSFERTHREADMANAGER_H
+#define TRANSFERTHREADMANAGER_H
+
+#include <QObject>
+#include <QList>
+#include <QMap>
+
+namespace IPConnect
+{
+
+class File;
+class Transfer;
+
+class TransferThreadManager : public QObject
+{
+	Q_OBJECT
+
+public:
+	explicit TransferThreadManager(QObject* parent = nullptr);
+	~TransferThreadManager();
+
+public Q_SLOTS:
+	void transferCreated(Transfer*);
+	void manualTransferCreated(Transfer*);
+	void acceptPending(qint16);
+	void rejectPending(qint16);
+
+	void requested();
+	void destroyTransfer();
+
+Q_SIGNALS:
+	void requestedTransfer(File);
+	void transferRemoved(qint16);
+	void accepted(qint16);
+	void rejected(qint16);
+
+protected:
+	QMap<qint16,Transfer*> m_transferList;
+
+	void removeTransfer(Transfer*);
+
+};
+
+}
+
+#endif
