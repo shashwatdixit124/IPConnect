210e596073cd83ee1b5ce34b0aeb5b12605f3670
Shashwat Dixit <shashwatdixit124@gmail.com>
2017-06-20 22:23:17 +0530

    ICryptEngine Interfaces that will be available everywhere for encrypting or decrypting data of messages as well as transfers

---
+++ src/lib/interfaces/icryptengine.h
@@ -0,0 +1,47 @@
+/*
+ *   This file is part of IPConnect
+ *   Copyright 2017 Shashwat Dixit <shashwatdixit124@gmail.com>
+ * 
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 3 of the
+ *   License, or (at your option) any later version.
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef ICRYPTENGINE_H
+#define ICRYPTENGINE_H
+
+#include <QObject>
+
+namespace IPConnect
+{
+
+class ICryptEngine : public QObject
+{
+	Q_OBJECT
+
+public:
+	virtual QByteArray publicKey() = 0;
+	virtual QByteArray privateKey() = 0;
+
+	virtual bool encryptData(QByteArray& , QByteArray& , QByteArray&) = 0;
+	virtual bool decryptData(QByteArray& , QByteArray&) = 0;
+
+protected:
+	explicit ICryptEngine(QObject* parent = nullptr);
+	~ICryptEngine();
+};
+
+}
+
+#endif
